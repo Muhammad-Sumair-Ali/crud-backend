f948b0fb61a722d5736e933b098cc84f1e4795ad
Muhammad-Sumair-Ali <sumairjattoi@gmail.com>
2024-09-17 17:31:58 +0500

    updated

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const dbCoonection = require('./config/database')
 const app = express()   
+const cors= require('cors')
 require('dotenv').config()
 const router = require('./routes/CRUD')
 
 const PORT = process.env.PORT;
 dbCoonection();
-
+app.use(cors())         
 app.use(express.json())
 app.use("/api" ,router)
 
